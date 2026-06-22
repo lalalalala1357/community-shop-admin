8f1a908ee8e4c72d930b6ec164dc6e7e98647812
陳立綸 <215938296+lalalalala1357@users.noreply.github.com>
2026-06-22 22:53:18 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAY7paruzZCSt5IIelL8UgAk76arA9P3-I",
+  authDomain: "community-group-buy-6d7ef.firebaseapp.com",
+  projectId: "community-group-buy-6d7ef",
+  storageBucket: "community-group-buy-6d7ef.firebasestorage.app",
+  messagingSenderId: "820713502218",
+  appId: "1:820713502218:web:f48b1f7a3cbc3d039391f3"
+};
---
+++ firebase.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  serverTimestamp,
+  runTransaction
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  serverTimestamp,
+  runTransaction,
+  ref,
+  uploadBytes,
+  getDownloadURL
+};
